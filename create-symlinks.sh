e711fef81ff9304c34813e15209f973fe5d9337d
Dmitri Fedotov <dmitri.fedotov@sap.com>
2019-04-25 00:02:28 +0300

    -- bin alias

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -8,9 +8,11 @@ ln -sf ~/dotfiles/.config ~/.config
 
 # Remove old bins
 sudo rm -rf /usr/local/bin/kubectl > /dev/null 2>&1
+sudo rm -rf /usr/local/bin/openstack > /dev/null 2>&1
 
 # Symlink new bins
 ln -sf ~/dotfiles/bin/kubectl /usr/local/bin/kubectl
+ln -sf ~/dotfiles/bin/openstack /usr/local/bin/openstack
 
 # Set fish as the default shell
 chsh -s /usr/local/bin/fish
